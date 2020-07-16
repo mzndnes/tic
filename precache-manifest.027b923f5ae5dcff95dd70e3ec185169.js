48fd97d9d7e07c43d823a1e04a4ac7c2510124ca
dnes <mzn.dnes@gmail.com>
2020-07-16 10:42:10 +0545

    Updates

---
+++ precache-manifest.027b923f5ae5dcff95dd70e3ec185169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7afbdc642072755615a104c34c4302f",
+    "url": "/tic/index.html"
+  },
+  {
+    "revision": "f4a74839715c09f855fc",
+    "url": "/tic/static/css/main.7064ff8d.chunk.css"
+  },
+  {
+    "revision": "28144619b1ff1b6c3734",
+    "url": "/tic/static/js/2.49031cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic/static/js/2.49031cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a74839715c09f855fc",
+    "url": "/tic/static/js/main.b98c2643.chunk.js"
+  },
+  {
+    "revision": "48976c7e4901a9219b93",
+    "url": "/tic/static/js/runtime-main.fce67067.js"
+  }
+]);
